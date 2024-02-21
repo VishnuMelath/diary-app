7e9aac6860af4d4c367d2733c3573e993d6b9c2d
vishnu <vishnumeloth@gmail.com>
2024-02-21 16:09:01 +0530

    restore

--- lib/widgets/custom_sliverlist.dart
+++ lib/widgets/custom_sliverlist.dart
@@ -18,11 +18,13 @@ List<Widget> customSliverList(
     required ValueNotifier keynotfierdiary,
     required BuildContext context,
     required int barindex}) {
+     
   List<Widget> list = [];
   for (var index in keys) {
+     ValueNotifier<bool> visible=ValueNotifier(false);
     DateTime date = entryMap[index]!.date;
     list.add(Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
       child: Material(
         elevation: 10,
         borderRadius: BorderRadius.circular(10),
@@ -36,114 +38,134 @@ List<Widget> customSliverList(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              ListTile(
-                title: Text(
-                  entryMap[index]!.title,
-                  style: GoogleFonts.blackOpsOne(fontSize: 20),
-                ),
-                subtitle:
-                    Text('${month[date.month]} ${date.day} , ${date.year}'),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ValueListenableBuilder(
-                        valueListenable: favnotifier,
-                        builder: (context, fav, child) {
-                          return entryMap[index]!.fav == false
-                              ? IconButton(
-                                  onPressed: () async {
-                                    entryMap[index]!.fav = true;
-                                    await EntryCRUD()
-                                        .updateEntry(index, entryMap[index]!);
-                                    entryMap[index]!.fav = true;
-                                    favnotifier.value = !favnotifier.value;
-                                  },
-                                  icon: const Icon(Icons.favorite_border))
-                              : IconButton(
-                                  onPressed: () async {
-                                    entryMap[index]!.fav = false;
-                                    await EntryCRUD()
-                                        .updateEntry(index, entryMap[index]!);
-                                    entryMap[index]!.fav = false;
-                                    favnotifier.value = !favnotifier.value;
-                                    Future.delayed(
-                                        const Duration(milliseconds: 70),
-                                        search);
-                                  },
-                                  icon: const Icon(Icons.favorite));
-                        }),
-                    PopupMenuButton(
-                      onSelected: (value) async {
-                        if (value == 'edit') {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => EditEntry(
-                                  entryval: entryMap[index]!,
-                                  index: index,
-                                  barindex: barindex,
-                                ),
-                              ));
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: Text('Delete'),
-                               content: Text('are you sure?'),
-                              actions: [
-                                customButton(
-                                    onTap: () async {
-                                      await EntryCRUD().deleteEntry(index);
-                                      keys.remove(index);
-                                      keynotfierdiary.value = [...keys];
-                                      search;
-                                      Navigator.of(context).pop();
+              GestureDetector(
+                onTap: () {
+                  visible.value=!visible.value;
+                },
+                child: ListTile(
+                  title: Text(
+                    entryMap[index]!.title,
+                    style: GoogleFonts.blackOpsOne(fontSize: 20),
+                  ),
+                  subtitle:
+                      Text('${month[date.month]} ${date.day} , ${date.year}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ValueListenableBuilder(
+                          valueListenable: favnotifier,
+                          builder: (context, fav, child) {
+                            return entryMap[index]!.fav == false
+                                ? IconButton(
+                                    onPressed: () async {
+                                      entryMap[index]!.fav = true;
+                                      await EntryCRUD()
+                                          .updateEntry(index, entryMap[index]!);
+                                      entryMap[index]!.fav = true;
+                                      favnotifier.value = !favnotifier.value;
                                     },
-                                    text: 'confirm',
-                                    buttoncolor: const MaterialStatePropertyAll(
-                                        canvasColor),
-                                    textcolor: white),
-                                customButton(
-                                    onTap: () async {
-                                      Navigator.of(context).pop();
+                                    icon: const Icon(Icons.favorite_border))
+                                : IconButton(
+                                    onPressed: () async {
+                                      entryMap[index]!.fav = false;
+                                      await EntryCRUD()
+                                          .updateEntry(index, entryMap[index]!);
+                                      entryMap[index]!.fav = false;
+                                      favnotifier.value = !favnotifier.value;
+                                      Future.delayed(
+                                          const Duration(milliseconds: 70),
+                                          search);
                                     },
-                                    text: 'cancel',
-                                    buttoncolor:
-                                        const MaterialStatePropertyAll(light),
-                                    textcolor: white)
-                              ],
-                            ),
-                          );
-                        }
-                      },
-                      padding: const EdgeInsets.all(0),
-                      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                        const PopupMenuItem(
-                          value: 'edit',
-                          child: Text('edit'),
-                        ),
-                        const PopupMenuItem(
-                          value: 'delete',
-                          child: Text('delete'),
-                        ),
-                      ],
-                    )
-                  ],
+                                    icon: const Icon(Icons.favorite));
+                          }),
+                      PopupMenuButton(
+                        onSelected: (value) async {
+                          if (value == 'edit') {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => EditEntry(
+                                    entryval: entryMap[index]!,
+                                    index: index,
+                                    barindex: barindex,
+                                  ),
+                                ));
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: Text('Delete'),
+                                 content: Text('are you sure?'),
+                                actions: [
+                                  customButton(
+                                      onTap: () async {
+                                        await EntryCRUD().deleteEntry(index);
+                                        keys.remove(index);
+                                        keynotfierdiary.value = [...keys];
+                                        search;
+                                        Navigator.of(context).pop();
+                                      },
+                                      text: 'confirm',
+                                      buttoncolor: const MaterialStatePropertyAll(
+                                          canvasColor),
+                                      textcolor: white),
+                                  customButton(
+                                      onTap: () async {
+                                        Navigator.of(context).pop();
+                                      },
+                                      text: 'cancel',
+                                      buttoncolor:
+                                          const MaterialStatePropertyAll(light),
+                                      textcolor: white)
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                        padding: const EdgeInsets.all(0),
+                        itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                          const PopupMenuItem(
+                            value: 'edit',
+                            child: Text('edit'),
+                          ),
+                          const PopupMenuItem(
+                            value: 'delete',
+                            child: Text('delete'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
-                child: ReadMoreText(
-                  entryMap[index]!.entry,
-                  trimLines: 1,
-                  trimMode: TrimMode.Line,
-                  style: const TextStyle(
-                      fontSize: 15, color: dark, fontWeight: FontWeight.w300),
-                ),
-              ),
-              if (entryMap[index]!.images.isNotEmpty)
-                showImage(entryMap[index]!.images, context)
+              ValueListenableBuilder(
+                valueListenable: visible,
+                builder: (context,value,_) {
+                  return Visibility(
+                    visible: value,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [ const Divider(),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
+                      child: ReadMoreText(
+                        entryMap[index]!.entry,
+                        trimLines: 1,
+                        trimMode: TrimMode.Line,
+                        style: const TextStyle(
+                            fontSize: 15, color: dark, fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    if (entryMap[index]!.images.isNotEmpty)
+                      showImage(entryMap[index]!.images, context)],
+                    ),
+                  );
+                }
+              )
+              ,
+             
             ],
           ),
         ),
